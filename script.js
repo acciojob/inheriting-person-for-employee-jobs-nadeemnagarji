5fcbb382359f8788a93adfe2acf36fdba07d6063
nadeemnagarji <129616695+nadeemnagarji@users.noreply.github.com>
2023-10-14 18:55:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+		this.name = name
+		this.age = age
+}
+Person.prototype.greet = function(){
+		console.log(``Hello, my name is ${this.name} and I am ${this.age} years old.``)
+}
 
 function Employee(name, age, jobTitle) {}
 
